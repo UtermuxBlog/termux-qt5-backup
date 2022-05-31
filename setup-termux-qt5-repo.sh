3d99e7f0677f510e14f0d8c83a5f5b7808a339b5
2096779623 <57583560+2096779623@users.noreply.github.com>
2022-05-31 13:47:54 +0800

    Update setup-termux-qt5-repo.sh

--- setup-termux-qt5-repo.sh
+++ setup-termux-qt5-repo.sh
@@ -7,3 +7,4 @@ rm $PREFIX/etc/apt/sources.list.d/qt5.list > /dev/null 2>&1
 rm $PREFIX/etc/apt/trusted.gpg.d/UtermuxBlog.gpg > /dev/null 2>&1
 echo deb https://utermuxblog.github.io/termux-qt5-backup termux qt5 > $PREFIX/etc/apt/sources.list.d/qt5.list
 wget -qO $PREFIX/etc/apt/trusted.gpg.d/UtermuxBlog.gpg https://utermuxblog.github.io/termux-qt5-backup/UtermuxBlog.gpg
+pkg up -y
